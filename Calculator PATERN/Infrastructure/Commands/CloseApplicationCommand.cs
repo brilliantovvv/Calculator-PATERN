92e48248a61a4f9a7309377cfcc9405e1e2e9606
Alex Brilliantov <Brilliantowww@gmail.com>
2020-07-05 13:10:11 +0300

    Кнопка выхода Стандартное подключение

---
+++ Calculator PATERN/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,23 @@
+﻿using Calculator_PATERN.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Calculator_PATERN.Infrastructure.Commands
+{
+    class CloseApplicationCommand : BaseCommand
+    {
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object parameter)
+        {
+             Application.Current.Shutdown();
+        }
+    }
+}
