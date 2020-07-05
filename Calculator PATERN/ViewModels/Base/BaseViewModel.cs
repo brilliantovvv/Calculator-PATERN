78f4ab1b28d13bfb58bffab69977db73429a8014
Alex Brilliantov <Brilliantowww@gmail.com>
2020-07-05 08:28:50 +0300

    Добавляем в базовый класс вьюМодели метод для предотвращение кольцевых обновлений

--- Calculator PATERN/ViewModels/Base/BaseViewModel.cs
+++ Calculator PATERN/ViewModels/Base/BaseViewModel.cs
@@ -3,15 +3,26 @@ using System.Runtime.CompilerServices;
 
 namespace Calculator_PATERN.ViewModels.Base
 {
-    // сообщает клиенту об изменениях свойств
+   
     internal abstract class BaseViewModel : INotifyPropertyChanged
     {
-
+        // сообщает клиенту об изменениях свойств
         public event PropertyChangedEventHandler PropertyChanged;
         public virtual void OnPropertyChanged([CallerMemberName] string prop = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
         }
 
+        // предотвращение кольцевых обновлений
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string prop = null)
+        {
+            if (Equals(field, value))
+            {
+                return false;
+            }
+            field = value;
+            OnPropertyChanged(prop);
+            return true;
+        }
     }
 }
